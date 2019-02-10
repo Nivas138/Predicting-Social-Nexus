768856a4d7ec4923e9464a7f7370f95074b9c063
Nivas Ganesan <38051335+Nivas138@users.noreply.github.com>
2019-02-10 19:16:18 +0530

    To Import Packages

---
+++ tweet_grasping_org.py
@@ -0,0 +1,5 @@
+import argparse  #to parse the arguments in command line 
+from urllib.parse import urlparse #urllib python module for opening URL , urlib.parse to break the url to components , 
+import urllib
+import csv
+import tweepy 
